5df95ed8f2d4cd6701224d72dc6f2e1f6e113e3e
felle64 <felle641@gmail.com>
2023-04-03 15:18:13 +0200

    works :)

--- blockchain-test-1/src/App.js
+++ blockchain-test-1/src/App.js
@@ -1,33 +1,39 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import Web3 from 'web3';
+import { PERSON_LIST_ABI, PERSON_LIST_ADDRESS } from './config';
 
 function App() {
   const [account, setAccount] = useState();
 
   useEffect(() => {
-    const getAccounts = async () =>
-    {
-      const web3 = new Web3(Web3.givenProvider || "https://localhost:7545")
-    const loggedInAs = await web3.eth.getAccounts();
-    console.log(loggedInAs);
-    setAccount(loggedInAs[0]);
-    }
+    const getAccounts = async () => {
+      const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
+      const loggedInAs = await web3.eth.getAccounts();
+      setAccount(loggedInAs[0]);
+      console.log(loggedInAs);
+
+      const personContract = new web3.eth.Contract(
+        PERSON_LIST_ABI, 
+        PERSON_LIST_ADDRESS
+        );
+
+      let indexes = await personContract.methods.getIndexList().call();
+      console.log(indexes);
+
+    };
 
     console.log("TEST");
     if (account) return;
     getAccounts();
-
-    
-    }
-  )
+  }, []);
 
   return (
     <div className="App">
       <p> Account: {account}</p>
-      <p>WAH</p>
     </div>
   );
 }
 
 export default App;
+
